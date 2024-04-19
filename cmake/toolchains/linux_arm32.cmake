0909fcfca03d2d84eb4595851530f878d311f141
Salakar <mike.diarmid@gmail.com>
2024-04-19 16:41:28 +0100

    -

--- cmake/toolchains/linux_arm32.cmake
+++ cmake/toolchains/linux_arm32.cmake
@@ -13,6 +13,8 @@
 # limitations under the License.
 
 # Toolchain file for building ARMv7 applications
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
 set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
 set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
 
